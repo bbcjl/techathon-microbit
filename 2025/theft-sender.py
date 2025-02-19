51a82f2cb0fb63262a7942504538b600c3c149a6
James Livesey <james.livesey@bbc.co.uk>
2025-02-19 10:48:15 +0000

    Add sleep period for theft detection to give time to settle

--- 2025/theft-sender.py
+++ 2025/theft-sender.py
@@ -1,12 +1,14 @@
 from microbit import *
 import radio
 
+sleep(3000)
+
 display.show(Image.HAPPY)
 
 radio.config(channel=0, power=7) # Channel to use from number on handout
 
 while True:
-    if accelerometer.get_strength() > 1_500:
+    if accelerometer.get_strength() > 1500:
         display.show(Image.ANGRY)
 
         radio.send("Stolen")
